37e38c0e635a6de174fd3551a3d9c8e6bf2dbdda
Azeezat <busariazeezat@gmail.com>
2021-06-23 03:39:14 +0100

    Email and Username Validation update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -25,8 +25,8 @@ class RegisterController extends Controller
      { 
          $this ->validate($request, [
              'name'=> 'required|max:255',
-             'username'=> 'required|max:255',
-             'email'=> 'required|email|max:255',
+             'username'=> 'required|max:255 |unique:users',
+             'email'=> 'required|email|max:255 |unique:users',
              'password' => 'required|confirmed',
          ]);
 
